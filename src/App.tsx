684d190a63264ba16900ef6dca9ba1f61f1bd565
Mohit kumar <mk513147@gmail.com>
2025-03-11 14:18:30 +0530

    remove unused CSS imports from App and main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Login from "./pages/Login/Login";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
 import App from "./App.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </Provider>
-  </StrictMode>
+	<StrictMode>
+		<Provider>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</Provider>
+	</StrictMode>
 );
